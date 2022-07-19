123c0021013baf657715049ac207339118a91cc8
WooSeok <wooseokbird@gmail.com>
2022-07-19 17:22:05 +0900

    add 배민

---
+++ delivery-practice/delivery-practice/Readme.md
@@ -0,0 +1,10 @@
+# 배달의 민족 Menu UI 
+
+<img width="764" alt="스크린샷 2022-07-09 오후 12 11 45" src="https://user-images.githubusercontent.com/55547933/178089444-37d6bba8-e88c-4c48-b572-93480efc937f.png">
+
+
+
+# 학습내용
+
+1. Stack view 를 활용하여 auto layout 적용 
+2. Iphone 8 , Iphone 11, Iphone 13 Pro max 각각의 디바이스에서 적용
---
+++ delivery-practice/delivery-practice/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  delivery-practice
+//
+//  Created by useok on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    @IBOutlet weak var AdImageVIew: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+//    @IBAction func chageImage(_ sender: UIButton) {
+//        let ImageList: [String] = ["cake","churros","doughnut","icecream"]
+//
+//        let nameImage=ImageList[Int.random(in: 0...ImageList.count-1)]
+//        AdImageVIew.image = UIImage(named: nameImage)
+//
+//
+//
+//    }
+    
+}
+
